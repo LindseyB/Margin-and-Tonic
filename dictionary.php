c109465825a7dffd9783a866da59cd8962c47678
Joe Frambach <joeframbach@energylegs.localdomain>
2011-04-03 06:45:21 +0800

    We have a 'dictionary' now\! It needs php and curl.

---
+++ dictionary.php
@@ -0,0 +1,12 @@
+<?php
+
+$url = 'http://www.google.com/search';
+$querystring = http_build_query(array('q'=>'define:'.$_GET['q']));
+
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_URL, "$url?$querystring");
+$result = curl_exec($curl);
+curl_close($curl);
+
+echo $result;
